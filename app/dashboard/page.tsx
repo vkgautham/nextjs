e8f0b07d804da2296448d47f1e36d558d288167d
V Gautham Kumar <vkgautham@gmail.com>
2025-08-03 11:15:40 +0530

    Rebase database

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,9 @@
+import CardWrapper from '@/app/ui/dashboard/cards';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { lusitana } from '@/app/ui/fonts';
+import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
 
 export default async function Page() {
 
@@ -9,18 +14,18 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Suspense fallback={<CardsSkeleton />}>
+        <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
-        </Suspense> */}
+        </Suspense>
         Card Wrapper
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* <Suspense fallback={<RevenueChartSkeleton/>}>
+        <Suspense fallback={<RevenueChartSkeleton/>}>
           <RevenueChart />
         </Suspense>
         <Suspense fallback={<LatestInvoicesSkeleton/>}>
           <LatestInvoices />
-        </Suspense> */}
+        </Suspense>
         Chart and Invoices
       </div>
     </main>
